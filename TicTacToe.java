2c9737b5ae8534463033c5b601204a24b5e2d7ea
akinator001 <aakashgandhi1279@gmail.com>
2020-10-07 09:41:04 +0530

    [aakash]Refactored uc2 (created method for option)

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,10 @@
 package com.cp.tictactoe;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToe {
+	Scanner sc = new Scanner(System.in);
 	char[] board ;
 	
 	TicTacToe(){
@@ -10,4 +12,16 @@ public class TicTacToe {
 		Arrays.fill(board, ' ');
 	}
 	
+	public void chooseOption(){
+		System.out.println("Choose x or o :");
+		char letter  = sc.next().charAt(0);
+		if(letter == 'x') {
+			System.out.println("player have x to play ");
+			System.out.println("computer have o to play ");
+		}
+		else {
+			System.out.println("player have o to play ");
+			System.out.println("computer have x to play ");
+		}
+	}
 }
